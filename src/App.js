a6aed7d7df1d58f0a94c7ab67ef54541e42927b7
Your Name <parth.spatel871@gmail.com>
2021-08-21 16:10:21 -0400

    nav done almost

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import './App.css';
+// import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
+// import MainPage from './Pages/MainPage/MainPage';
+ import Nav from './Components/Nav/Nav';
+// import Login from './Pages/LoginPage/LoginPage';
+
+function App() {
+
+  return (
+
+    <Nav/>
+
+    // <Router>
+
+    //   <Nav />
+
+    //   <Switch>
+    //     <Route path="/" exact component={Login}  />
+    //     <Route path="/main" component={MainPage}  />
+    //   </Switch>
+
+    // </Router>
+  )
+}
+
+export default App;
---
+++ src/Components/Nav/Nav.js
@@ -0,0 +1,39 @@
+import './Nav.css';
+
+const Nav = () => {
+
+    return (
+
+        <nav>
+
+            <div className="logo">
+
+                <a href="/">
+                    <img src="" />
+                </a>
+                
+            </div>
+
+            <div className="links">
+
+                <a href="/">
+                    work.
+                </a>
+                
+                <a href="https://github.com/parth-p29?tab=repositories" target="_blank">
+                    about.
+                </a>
+
+                <a href="https://github.com/parth-p29?tab=repositories" target="_blank">
+                    resume.
+                </a>
+
+            </div>
+
+        </nav>
+
+    );
+
+}
+
+export default Nav;
