ef98821b45239e6cefc1c1ed95928d3d9afb69a1
Your Name <parth.spatel871@gmail.com>
2021-08-23 19:23:30 -0400

    landing done

--- src/App.js
+++ src/App.js
@@ -14,22 +14,65 @@ function App() {
 
 
     <Nav/>
-
-    <Intro/>
     
-    <Particles
-      params={{
-        particles: {
-          number: {
-            value: 200,
-            density: {
-              enable: true,
-              value_area: 1000,
-            }
+    {/* <Particles
+    canvasClassName="example"
+    params={{
+	    "particles": {
+	        "number": {
+	            "value": 160,
+	            "density": {
+	                "enable": false
+	            }
+	        },
+          "color":{
+            "value": "random"
           },
-        },
-      }}
-    />
+	        "size": {
+	            "value": 3,
+	            "random": true,
+	            "anim": {
+	                "speed": 4,
+	                "size_min": 0.3
+	            }
+	        },
+	        "line_linked": {
+	            "enable": false
+	        },
+	        "move": {
+	            "random": true,
+	            "speed": 1,
+	            "direction": "top",
+	            "out_mode": "out"
+	        }
+	    },
+	    "interactivity": {
+	        "events": {
+	            "onhover": {
+	                "enable": true,
+	                "mode": "bubble"
+	            },
+	            "onclick": {
+	                "enable": true,
+	                "mode": "repulse"
+	            }
+	        },
+	        "modes": {
+	            "bubble": {
+	                "distance": 250,
+	                "duration": 2,
+	                "size": 0,
+	                "opacity": 0
+	            },
+	            "repulse": {
+	                "distance": 400,
+	                "duration": 4
+	            }
+	        }
+	    }
+	}} /> */}
+
+<Intro/>
 
     </>
   )
--- src/Components/Intro/Intro.js
+++ src/Components/Intro/Intro.js
@@ -1,6 +1,7 @@
 import pic from '../../Assests/intropic.jpg';
 import './Intro.css';
 import Typewriter from 'typewriter-effect';
+import arrow from '../../Assests/down-arrow.png';
 
 const phrases = [
     "I'm Parth.",
@@ -12,102 +13,115 @@ const Intro = () => {
 
     return (
 
-        <div className="Intro">
+        <div className="container">
+            <div className="Intro">
 
-            <div className="info">
+                <div className="info">
 
-                <h1>
-                    Hey👋, I'm Parth!
-                    {/* <div className="typing">
-                        <Typewriter
+                    <h1>
+                        Hey, I'm Parth!
+                        {/* <div className="typing">
+                            <Typewriter
 
-                            onInit={(tw) => {
-                                
-                                    tw.typeString('Parth!')
-                                    tw.pauseFor(2000)
-                                    tw.deleteChars(6)
-                                    tw.pauseFor(1200)
-
-                                    //
-                                    tw.typeString('a software developer.')
-                                    tw.pauseFor(2000)
-                                    tw.deleteChars(19)
-                                    tw.pauseFor(1200)
-
-                                    //
-
-                                    tw.typeString('designer.')
-                                    tw.pauseFor(2000)
-                                    tw.deleteChars(11)
-                                    tw.pauseFor(1200)
-
-                                    //
-
-                                    tw.typeString('an explorer.')
-                                    tw.pauseFor(2000)
-                                    tw.deleteChars(11)
-                                    tw.pauseFor(1200)
-
-                                    //
-
-                                    tw.typeString(' problem-solver.')
-                                    tw.pauseFor(2000)
-                                    tw.deleteChars(19)
-                                    tw.pauseFor(1200)
-
-                                    //
+                                onInit={(tw) => {
                                     
+                                        tw.typeString('Parth!')
+                                        tw.pauseFor(2000)
+                                        tw.deleteChars(6)
+                                        tw.pauseFor(1200)
+
+                                        //
+                                        tw.typeString('a software developer.')
+                                        tw.pauseFor(2000)
+                                        tw.deleteChars(19)
+                                        tw.pauseFor(1200)
+
+                                        //
+
+                                        tw.typeString('designer.')
+                                        tw.pauseFor(2000)
+                                        tw.deleteChars(11)
+                                        tw.pauseFor(1200)
+
+                                        //
+
+                                        tw.typeString('an explorer.')
+                                        tw.pauseFor(2000)
+                                        tw.deleteChars(11)
+                                        tw.pauseFor(1200)
+
+                                        //
+
+                                        tw.typeString(' problem-solver.')
+                                        tw.pauseFor(2000)
+                                        tw.deleteChars(19)
+                                        tw.pauseFor(1200)
+
+                                        //
+                                        
+                                        .start();
+
+                                }}
+
+                            />
+                        </div> */}
+                    </h1>
+                    
+                    <h5>I study&nbsp;<a class="link" target="_blank" href="https://syde.social/">Systems Design Engineering</a>&nbsp;at the University of Waterloo.</h5>
+                    <h5>I have a vast interest in software systems and aspire to create viable solutions aimed towards addressing the problems faced by various people.</h5>
+                    <h5>Previously I worked as a&nbsp;
+
+                    <div className="typing">
+                            <Typewriter
+                                options={{
+                                    loop: true,
+                                    cursor: "✍️",
+                                    cursorClassName:"cursor",
+                                    delay: 50,
+                                    deleteSpeed: 10
+
+                                }}
+
+                                onInit={(tw) => {
+                                    
+                                    tw.typeString('<a class="job" target="_blank" href="https://www.ceridian.com/">Software Developer @ Ceridian</a>')
+                                    tw.pauseFor(1000)
+                                    tw.deleteChars(29)
+                                    tw.pauseFor(200)
+
+                                    //
+                                    tw.typeString('<a class="job" target="_blank" href="https://www.reebee.com/">Backend Software Engineer @ Reebee</a>')
+                                    tw.pauseFor(1000)
+                                    tw.deleteChars(34)
+                                    tw.pauseFor(200)
+
+                                    //
+
                                     .start();
 
-                            }}
+                                }}
 
-                        />
-                    </div> */}
-                </h1>
-                
-                <p>I study&nbsp;<a href="https://syde.social/">Systems Design Engineering</a>&nbsp;at the University of Waterloo.</p>
-                <p>I have a vast interest in software systems and aspire to create viable solutions aimed towards addressing the problems faced by various people.</p>
-                <p>Previously I worked as a&nbsp;
+                            />
+                        </div>
+                    </h5>
 
-                <div className="typing">
-                        <Typewriter
-                            options={{
-                                loop: true
-                            }}
+                </div>
 
-                            onInit={(tw) => {
-                                
-                                tw.typeString('<a class="ceridian" href="https://www.ceridian.com/">Software Developer @ Ceridian</a>')
-                                tw.pauseFor(2000)
-                                tw.deleteChars(29)
-                                tw.pauseFor(1200)
+                <div className="image">
+                    
+                    <img src={pic} />
 
-                                //
-                                tw.typeString('<a class="reebee" href="https://www.reebee.com/">Backend Software Engineer @ Reebee</a>')
-                                tw.pauseFor(2000)
-                                tw.deleteChars(34)
-                                tw.pauseFor(1200)
-
-                                //
-
-                                .start();
-
-                            }}
-
-                        />
-                    </div>
-                </p>
+                </div>     
 
             </div>
 
-            <div className="image">
-                
-                <img src={pic} />
-
-            </div>
-
+            <div className="more-info">
+                <p>check out my work below.</p>
+                <img src={arrow} />
+            </div> 
 
         </div>
+
 
 
     )
