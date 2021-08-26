74757ff9b0db10bc4baef7dd713c6f51681051ed
Your Name <parth.spatel871@gmail.com>
2021-08-26 17:25:10 -0400

    ceridian stuff

--- src/Pages/Ceridian/Ceridian.js
+++ src/Pages/Ceridian/Ceridian.js
@@ -4,7 +4,25 @@ const Ceridian = () => {
 
     return (
 
-        <p>Ceridian</p>
+        <>
+            <div className="company-info">
+
+                <div className="company-title">
+                    <h1>Ceridian Internship</h1>
+                    <p>Interned as a Software Developer on the architecture team during Winter 2021</p>
+                    <p>Here are some pics of how it went (virtually)</p>
+                </div>
+
+
+
+                
+
+
+
+
+            </div>
+
+        </>
     );
 
 }
