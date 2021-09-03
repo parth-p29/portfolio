c8f89853aa72c077f113a5af893433cb44483621
Your Name <parth.spatel871@gmail.com>
2021-09-03 00:12:26 -0400

    intro responsive almost done

--- src/Components/Intro/Intro.js
+++ src/Components/Intro/Intro.js
@@ -69,7 +69,7 @@ const Intro = () => {
                     
                     <h5>I study&nbsp;<a class="link" target="_blank" href="https://syde.social/">Systems Design Engineering</a>&nbsp;at the University of Waterloo.</h5>
                     <h5>I have a vast interest in software systems and aspire to create viable solutions aimed towards solving the problems faced by all kinds of people.</h5>
-                    <h5>Previously I interned as a&nbsp;
+                    <h5 id="l">Previously I interned as a&nbsp;
 
                     <div className="typing">
                             <Typewriter
