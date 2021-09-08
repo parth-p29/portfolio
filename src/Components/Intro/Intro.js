dc910a7d5fa4564275bd0d187de4528b44791833
Your Name <parth.spatel871@gmail.com>
2021-09-08 11:42:50 -0400

    intro fix

--- src/Components/Intro/Intro.js
+++ src/Components/Intro/Intro.js
@@ -20,39 +20,38 @@ const Intro = () => {
                     <h5>I study&nbsp;<a class="link" target="_blank" href="https://syde.social/">Systems Design Engineering</a>&nbsp;at the University of Waterloo.</h5>
                     <h5>I have a vast interest in software systems and aspire to create viable solutions aimed towards solving the problems faced by all kinds of people.</h5>
                     <h5 id="l">Previously I interned as a&nbsp;
+                        <div className="typing">
+                                <Typewriter
+                                    options={{
+                                        loop: true,
+                                        cursor: "✍️",
+                                        cursorClassName:"cursor",
+                                        delay: 50,
+                                        deleteSpeed: 10
 
-                    <div className="typing">
-                            <Typewriter
-                                options={{
-                                    loop: true,
-                                    cursor: "✍️",
-                                    cursorClassName:"cursor",
-                                    delay: 50,
-                                    deleteSpeed: 10
+                                    }}
 
-                                }}
+                                    onInit={(tw) => {
+                                        
+                                        tw.typeString('<a class="job" target="_blank" href="https://www.ceridian.com/">Software Developer @ Ceridian</a>')
+                                        tw.pauseFor(1000)
+                                        tw.deleteChars(29)
+                                        tw.pauseFor(200)
 
-                                onInit={(tw) => {
-                                    
-                                    tw.typeString('<a class="job" target="_blank" href="https://www.ceridian.com/">Software Developer @ Ceridian</a>')
-                                    tw.pauseFor(1000)
-                                    tw.deleteChars(29)
-                                    tw.pauseFor(200)
+                                        //
+                                        tw.typeString('<a class="job" target="_blank" href="https://www.reebee.com/">Backend Software Engineer @ Reebee</a>')
+                                        tw.pauseFor(1000)
+                                        tw.deleteChars(34)
+                                        tw.pauseFor(200)
 
-                                    //
-                                    tw.typeString('<a class="job" target="_blank" href="https://www.reebee.com/">Backend Software Engineer @ Reebee</a>')
-                                    tw.pauseFor(1000)
-                                    tw.deleteChars(34)
-                                    tw.pauseFor(200)
+                                        //
 
-                                    //
+                                        .start();
 
-                                    .start();
+                                    }}
 
-                                }}
-
-                            />
-                        </div>
+                                />
+                            </div>
                     </h5>
 
                 </div>
